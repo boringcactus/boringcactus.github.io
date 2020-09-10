d1d65d5d64ea7fc1418c607d12c675113da9513a
Melody Horn <melody@boringcactus.com>
2020-09-09 18:37:29 -0600

    fuck HN all my homies hate HN

---
+++ fuck-hn.js
@@ -0,0 +1,4 @@
+// Hacker News is bad.
+if (document.referrer.startsWith("https://news.ycombinator.com")) {
+    document.location = "https://upload.wikimedia.org/wikipedia/commons/d/d4/Human_fart.wav"
+}
